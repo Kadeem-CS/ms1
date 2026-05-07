bb8ccc01aa868bc618dae0e954ce2d2c54545ad0
Kadeem-CS <kadeemcollins10@gmail.com>
2026-05-07 12:10:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__Kadeem__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name} from a GCP Microservice done by your first name and your lastname!"
+    Kadeem = os.environ.get("NAME", "World")
+    return f"Hello {Kadeem} from a GCP Microservice done by Kadeem Collins!"
 
-if __name__ == "__main__":
+if __Kadeem__ == "__main__":
     # Cloud Run provides a PORT environment variable
     port = int(os.environ.get("PORT", 8080))
     app.run(debug=True, host="0.0.0.0", port=port)
